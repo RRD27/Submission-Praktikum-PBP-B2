829dd20eab1d59a455e0404429b4f44e39e57b99
Raden Rico Dwianda <117699058+RRD27@users.noreply.github.com>
2024-11-21 21:07:22 +0700

    Add files via upload

---
+++ 24060122140184/submission-5/Pertemuan 5 PBP/show_server_time.php
@@ -0,0 +1,16 @@
+<?php require_once("header.php"); ?> <br>
+
+<div class="card">
+    <div class="card-header">Ajax Server Time</div>
+    <div class="card-body">
+        <!-- TODO: Membuat tombol Show Server Time -->
+         <button class="btn btn-success" onclick="get_server_time()">Show Server Time</button>
+
+        <br><br>
+        <!-- TODO: Membuat elemen untuk menampilkan server time -->
+         <div id="showtime"></div>
+
+    </div>
+</div>
+
+<?php require_once("footer.php"); ?>
